7364f30547bb57f2db65b02094108a062ad0e96e
Robert Sell <robertsell21@gmail.com>
2021-02-28 12:48:04 -0500

    feat: serialize store to session storage

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ import { configureStore } from "./redux";
 import { ThemeContainer } from "./containers";
 import { QuotesPage } from "./pages";
 
-const store = configureStore();
+const storageKey = "swanson-redux";
+const initialState = JSON.parse(
+  window.sessionStorage.getItem(storageKey) ?? "{}"
+);
+const store = configureStore(initialState);
+store.subscribe(() => {
+  const storeState = store.getState();
+  const serialziedStoreState = JSON.stringify(storeState);
+  window.sessionStorage.setItem(storageKey, serialziedStoreState);
+});
 
 function App() {
   return (
