c582c8477c9cfa156d629c9daa94df3d472f36e8
Robert Sell <robertsell21@gmail.com>
2021-02-28 12:52:31 -0500

    feat: add more views

--- src/views/quote-table-array.ts
+++ src/views/quote-table-array.ts
@@ -1,20 +1,27 @@
 import { ApplicationReduxStore } from "../redux";
 import { toPairs } from "lodash";
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
 
-export const quotesTableSelector = (state: ApplicationReduxStore) =>
-  state.quotesTable;
+export const quotesTableSelector = (
+  state: Pick<ApplicationReduxStore, "quotesTable">
+) => state.quotesTable;
 
 export const quotesList = createSelector(quotesTableSelector, (quotesTable) => {
-  const quotesArray = toPairs(quotesTable).map(([id, quote]) => quote);
+  const quotesArray = toPairs(quotesTable).map(([, quote]) => quote);
 
   return quotesArray;
 });
 
 export const voteCount = createSelector(quotesList, (listOfQuotes) => {
   const totalVotes = listOfQuotes.reduce((totalVotes, currentValue) => {
-    return (totalVotes += currentValue?.votes);
+    return (totalVotes += currentValue.votes);
   }, 0);
 
   return totalVotes;
 });
+
+export const fetchingQuotesProgress = (
+  state: Pick<ApplicationReduxStore, "fetchingQuotes">
+) => {
+  return state.fetchingQuotes;
+};
